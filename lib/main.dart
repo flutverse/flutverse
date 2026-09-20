a3d23891af89db9979f90a51895bc3b8694dbd58
Md. Ariful Islam <flutverse@gmail.com>
2026-09-21 02:08:44 +0600

    welcome

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Cache clear'))),
+      home: Scaffold(body: Center(child: Text('Welcome to Flutverse'))),
     );
   }
 }
