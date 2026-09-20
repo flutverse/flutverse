2ad06af16fbffc10691539857059f4b1dc384f83
Md. Ariful Islam <flutverse@gmail.com>
2026-09-20 23:38:26 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(body: Center(child: Text('Updated'))),
+      home: Scaffold(body: Center(child: Text('Updated more from  vs code'))),
     );
   }
 }
